7ae567783b85f3d775c91e5b4bda52497f2aedd9
callumccr@gmail.com <jenkins-x@googlegroups.com>
2020-03-12 22:31:39 +0000

    Gitlab README commit

--- label.tf
+++ label.tf
@@ -1,11 +1,10 @@
 module "label" {
-  source             = "git::ssh://git@github.com:Callumccr/tf-mod-label.git?ref=master"
-  context            = var.context
+  source             = "git::https://github.com/Callumccr/tf-mod-label.git?ref=master"
   namespace          = var.namespace
   environment        = var.environment
   name               = var.name
   attributes         = ["role"]
   delimiter          = "-"
   additional_tag_map = {} /* Additional attributes (e.g. 1) */
-  label_order        = ["namespace", "environment", "name", "attributes"]
+  label_order        = ["environment", "namespace", "name", "attributes"]
 }
