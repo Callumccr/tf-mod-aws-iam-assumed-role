80f5eed358c496ac2064152289d74e859342e120
callumccr@gmail.com <jenkins-x@googlegroups.com>
2020-03-14 19:06:07 +0000

    Gitlab README commit

--- main.tf
+++ main.tf
@@ -2,12 +2,12 @@ resource "aws_iam_role" "default" {
   count              = var.enabled ? 1 : 0
   name               = module.label.id
   path               = var.path
-  assume_role_policy = data.aws_iam_policy_document.default.json
+  assume_role_policy = data.aws_iam_policy_document.default.0.json
   tags               = module.label.tags
 }
 
 resource "aws_iam_role_policy_attachment" "default" {
   count      = var.enabled ? length(var.aws_policy_arns) : 0
-  role       = aws_iam_role.default.name
+  role       = aws_iam_role.default.0.name
   policy_arn = var.aws_policy_arns[count.index]
 }
