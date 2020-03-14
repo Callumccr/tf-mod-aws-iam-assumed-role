4629c733d02fcb22efc83e6d139cf61d9cac1112
callumccr@gmail.com <jenkins-x@googlegroups.com>
2020-03-14 19:18:55 +0000

    Gitlab README commit

--- label.tf
+++ label.tf
@@ -3,7 +3,7 @@ module "label" {
   namespace          = var.namespace
   environment        = var.environment
   name               = var.name
-  attributes         = merge(var.attributes, ["role"])
+  attributes         = concat(var.attributes, ["role"])
   delimiter          = "-"
   additional_tag_map = {} /* Additional attributes (e.g. 1) */
   label_order        = ["environment", "namespace", "name", "attributes"]
