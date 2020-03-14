46b570a9b0c3472b7d6a6cb5465d55d49ba05c25
callumccr@gmail.com <jenkins-x@googlegroups.com>
2020-03-14 19:07:17 +0000

    Gitlab README commit

--- outputs.tf
+++ outputs.tf
@@ -3,26 +3,26 @@
 # -----------------------------------------------------------------------------
 
 output "arn" {
-  value       = aws_iam_role.default.arn
+  value       = aws_iam_role.default.0.arn
   description = "The Amazon Resource Name (ARN) specifying the role."
 }
 
 output "create_date" {
-  value       = aws_iam_role.default.create_date
+  value       = aws_iam_role.default.0.create_date
   description = "The creation date of the IAM role."
 }
 
 output "description" {
-  value       = aws_iam_role.default.description
+  value       = aws_iam_role.default.0.description
   description = "The description of the role."
 }
 
 output "id" {
-  value       = aws_iam_role.default.id
+  value       = aws_iam_role.default.0.id
   description = "The name of the role."
 }
 
 output "unique_id" {
-  value       = aws_iam_role.default.unique_id
+  value       = aws_iam_role.default.0.unique_id
   description = "The stable and unique string identifying the role."
 }
